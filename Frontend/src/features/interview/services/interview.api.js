7a306c4b48d48a6374987bd4b8e0e533921b5f94
Abhijeet Chandra <kumarabhij72@gmail.com>
2026-09-18 16:30:41 +0530

    Fix multipart form upload

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -16,7 +16,11 @@ export const generateInterviewReport = async ({ jobDescription, selfDescription,
     formData.append("selfDescription", selfDescription)
     formData.append("resume", resumeFile)
 
-    const response = await api.post("/api/interview/", formData)
+    const response = await api.post("/api/interview/", formData, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        }
+    })
 
     return response.data
 
